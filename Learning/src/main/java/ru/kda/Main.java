a57f02616f5cb0c09b60c695565b65d5e3354194
Коротков Дмитрий Алексеевич <DAleKorotkov@sberbank.ru>
2025-10-09 17:17:09 +0300

    refaktoring

---
+++ Learning/src/main/java/ru/kda/Main.java
@@ -0,0 +1,7 @@
+package Learning.src.main.java.ru.kda;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Learning/src/main/java/ru/kda/lecture2/common/Utils.java
@@ -0,0 +1,7 @@
+package Learning.src.main.java.ru.kda.lecture2.common;
+
+public interface Utils
+{
+    boolean utilFunc1(String str);
+    boolean utilFunc2();
+}
