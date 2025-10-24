f6157db38794e490fe80fa832e765a66f1411370
Коротков Дмитрий Алексеевич <DAleKorotkov@sberbank.ru>
2025-10-24 09:41:29 +0300

    refactor

--- Learning2/src/main/java/ru/kda/lecture2/common/Utils.java
+++
@@ -1,7 +0,0 @@
-package ru.kda.lecture2.common;
-
-public interface Utils
-{
-    boolean utilFunc1(String str);
-    boolean utilFunc2();
-}
--- lecture3/src/main/java/ru/kda/oop_nice_code/Gender.java
+++
@@ -1,7 +0,0 @@
-package ru.kda.oop_nice_code;
-
-public enum Gender {
-
-    MALE,
-    FEMALE
-}
